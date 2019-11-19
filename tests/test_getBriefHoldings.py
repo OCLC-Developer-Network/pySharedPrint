0c5c925213b6096a544363588a27bf73ed7bb2c7
Coombs,Karen <coombsk@oclc.org>
2019-11-19 13:04:11 -0600

    Tests for getting brief holdings

---
+++ tests/test_getBriefHoldings.py
@@ -0,0 +1,42 @@
+import pytest
+import json
+import requests_mock
+import pandas
+import handler
+
+with open('tests/mocks/holdings.json', 'r') as myfile:
+    data=myfile.read()
+
+# parse file
+holdings_list = json.loads(data)
+
+with open('tests/mocks/no_holdings.json', 'r') as myfile:
+    data=myfile.read()
+
+# parse file
+no_holdings_list = json.loads(data)
+
+
+def test_getBriefHoldings(requests_mock):
+    oclcNumber = "2416076"
+    country = "US"
+    requests_mock.register_uri('GET', 'https://americas.api.oclc.org/discovery/v1/worldcat/bibs-holdings?oclcNumber=' + oclcNumber + '&heldInCountry=' + country, status_code=200, json=holdings_list)
+    bib = handler.getBriefHoldings(oclcNumber, country);
+    assert type(bib) is pandas.core.series.Series
+    assert bib[0] == '2416076'
+    assert bib[1] == 16005
+    assert len(bib[2].split(',')) == 16005 
+    assert bib[3] == 'success' 
+    
+def test_getBriefHoldings_NoResults(requests_mock):
+    oclcNumber = "2416076"
+    country = "CH"
+    requests_mock.register_uri('GET', 'https://americas.api.oclc.org/discovery/v1/worldcat/bibs-holdings?oclcNumber=' + oclcNumber + '&heldInCountry=' + country, status_code=200, json=no_holdings_list)
+    bib = handler.getBriefHoldings(oclcNumber, country);
+    assert type(bib) is pandas.core.series.Series
+    assert bib[0] == '2416076'
+    assert bib[1] == 246
+    assert bib[2] == ""
+    assert bib[3] == 'success'     
+    
+    
