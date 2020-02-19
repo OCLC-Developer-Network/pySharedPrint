42844d5b38add64daf8efdbfd8e046403cc7297a
Coombs,Karen <coombsk@oclc.org>
2020-02-18 21:01:45 -0600

    added test for commandline arguments

--- tests/test_getData.py
+++ tests/test_getData.py
@@ -1,10 +1,11 @@
 import pytest
 import sys
 import getData
+from src import handle_files, process_data, make_requests
 
 def testProcessArgs(mocker):
-    mocker.patch('sys.argv', ["", "--itemFile", "samples/oclcNumbers.csv", "--operation", "retrieveMergedOCLCNumbers", "--outputDir", "samples/results"])    
+    mocker.patch('sys.argv', ["", "--itemFile", "samples/oclc_numbers.csv", "--operation", "retrieveMergedOCLCNumbers", "--outputDir", "samples/results"])    
     args = getData.processArgs()
-    assert args.itemFile == "samples/oclcNumbers.csv"
+    assert args.itemFile == "samples/oclc_numbers.csv"
     assert args.operation == "retrieveMergedOCLCNumbers"
-    assert args.outputDir == "samples/results"                                
+    assert args.outputDir == "samples/results"                   
